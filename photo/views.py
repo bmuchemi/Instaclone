395d901b8ec5ea369372ec190a941765895b3ccc
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2022-06-05 20:49:11 +0300

    add a feed view

--- photo/views.py
+++ photo/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Image
 
 # Create your views here.
 
-    
+def feed(request):
+    pictures = Image.objects.all()
+    number = Comment.objects.count()
+    return render(request, 'feed.html', {'pictures': pictures, 'number': number})
