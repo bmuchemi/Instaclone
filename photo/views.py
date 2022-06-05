3429a1d85b6e0d9a07725ba3b8d4ec50a044718d
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2022-06-06 00:22:35 +0300

    updaye comments on views.py

--- photo/views.py
+++ photo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import Image
+from .models import Image,Comment
 from django.contrib.auth.models import User
 from .forms import uploadForm
 
@@ -33,3 +33,33 @@ def new_image(request):
     else:
         form = uploadForm()
     return render(request, 'new_image.html', {'form':form})
+
+def comments(request, image_id):
+    current_user = request.user.profile
+    post = Image.objects.filter(id=id)
+
+    if request.method == 'POST':  
+        form = commentForm(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.name = current_user
+            comment.related_post = post
+            comment.save()
+        return redirect('comments')
+
+    else:
+        form = commentForm()
+
+        maoni = Comment.objects.filter(related_post=id).all()
+
+    return render(request, 'comments.html', {'form':form, 'maoni':maoni})
+
+
+def like_post(request, post_id):
+    current_user = request.user
+    img = Image.objects.get(id=post_id)
+    if img.likes.filter(id=current_user.id).exists():
+        img.likes.remove(current_user)
+    else:
+        img.likes.add(current_user)
+    return redirect('feed')
