d84e1697b975d914fe1ff72eccb762e329d7b73e
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2022-06-05 20:35:47 +0300

    Database configurations

--- Instaclone/settings.py
+++ Instaclone/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'photo.apps.PhotoConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,14 @@ WSGI_APPLICATION = 'Instaclone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Instaclone',
+        'USER': 'bmuchemi',
+        'PASSWORD': 'BENJA9262',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    caption = models.CharField(max_length=500)
+    likes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
